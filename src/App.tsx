de8c18af74916abd4cd64765005f5f3953333b9d
Pratham Gupta <prathamgupta.pg1@gmail.com>
2023-04-10 17:19:41 +0530

    Initialized project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <div className="w-screen h-screen bg-gray-600">
+            <h1 className="text-center text-xl sm:text-2xl md:text-3xl lg:text-4xl p-4">
+                GDSC Frontend Task 1
+            </h1>
+        </div>
+    );
+}
+
+export default App;
